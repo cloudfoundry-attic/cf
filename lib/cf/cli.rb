d7ee3b34b961473935206cd79ba1d3670a5903f7
Alex Lobascio & Sheel Choksi <pair+alobascio+schoksi@pivotallabs.com>
2013-04-03 16:37:41 -0700

    update help instructions to be more explicit about removal of --ask-org and --ask-space [#47410815]

--- lib/cf/cli.rb
+++ lib/cf/cli.rb
@@ -86,11 +86,11 @@ module CF
       check_logged_in
 
       unless client.current_organization
-        fail "Please select an organization with 'cf target --ask-org'."
+        fail "Please select an organization with 'cf target --organization ORGANIZATION_NAME'. (Get organization names from 'cf orgs'.)"
       end
 
       unless client.current_space
-        fail "Please select a space with 'cf target --ask-space'."
+        fail "Please select a space with 'cf target --space SPACE_NAME'. (Get space names from 'cf spaces'.)"
       end
     end
 
