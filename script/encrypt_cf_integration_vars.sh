adad1023fe750dceda93de5c7dc1687cab3cb799
Dennis Schmidt & Ian Baker <pair+dschmidt+ibaker@pivotallabs.com>
2013-04-04 15:09:43 -0700

    added script for encrypting travis environment variables

---
+++ script/encrypt_cf_integration_vars.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# argument is the path to the script that sets all the vars
+source $1
+
+for var in `grep -v '^#' $1 | cut -d= -f1`
+do
+  value=`eval echo "\\$$var"`;
+  echo \#$var
+  encrypted=`travis encrypt $var=$value 2>/dev/null`
+  echo secure:$encrypted
+  echo
+done
+
+echo "*****************"
+echo "paste these into .travis.yml in place of the ones that are already there"
+echo "remember to remove those pesky newlines"
+echo "*****************"
