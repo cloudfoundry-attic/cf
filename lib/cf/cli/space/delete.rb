6cf7b12cfe3f05152d1c4dfe858a3ec6f0ef4920
Alex Lobascio & Sheel Choksi <pair+alobascio+schoksi@pivotallabs.com>
2013-04-10 13:45:08 -0700

    Show CC error when deleting a space that has dependents
    backfill space deletion unit tests, refactor code and remove --ask-space from space deletion
    recursive deletion is not currently implemented

--- lib/cf/cli/space/delete.rb
+++ lib/cf/cli/space/delete.rb
@@ -5,16 +5,17 @@ module CF::Space
     desc "Delete a space and its contents"
     group :spaces
     input :organization, :desc => "Space's organization",
-          :aliases => ["--org", "-o"], :from_given => by_name(:organization),
-          :default => proc { client.current_organization }
+      :aliases => ["--org", "-o"], :from_given => by_name(:organization),
+      :default => proc { client.current_organization }
     input :spaces, :desc => "Spaces to delete", :argument => :splat,
-          :singular => :space, :from_given => space_by_name
+      :singular => :space, :from_given => space_by_name
     input :recursive, :desc => "Delete recursively", :alias => "-r",
-          :default => false, :forget => true
+      :default => false, :forget => true
     input :warn, :desc => "Show warning if it was the last space",
-          :default => true
+      :default => true
     input :really, :type => :boolean, :forget => true, :hidden => true,
-          :default => proc { force? || interact }
+      :default => proc { force? || interact }
+
     def delete_space
       spaces = input[:spaces, org]
 
@@ -23,65 +24,26 @@ module CF::Space
       spaces.each do |space|
         next unless input[:really, space]
 
-        next unless clear_space(space)
+        deleted_current ||= (space == client.current_space)
 
-        deleted_current ||= space == client.current_space
-
-        with_progress("Deleting space #{c(space.name, :name)}") do
-          space.delete!
+        begin
+          with_progress("Deleting space #{c(space.name, :name)}") do
+            space.delete!
+          end
+        rescue CFoundry::APIError => boom
+          line
+          line c(boom.description, :bad)
+          line c("If you want to delete the space along with all dependent objects, rerun the command with the #{b("'--recursive'")} flag.", :bad)
         end
       end
 
-      org.invalidate!
-
-      if org.spaces.empty?
-        return unless input[:warn]
-
+      if deleted_current
         line
-        line c("There are no longer any spaces in #{b(org.name)}.", :warning)
-        line "You may want to create one with #{c("create-space", :good)}."
-      elsif deleted_current
-        invalidate_client
-        invoke :target, :organization => client.current_organization
+        line c("The space that you were targeting has now been deleted. Please use #{b("`cf target -s SPACE_NAME`")} to target a different one.", :warning)
       end
-    end
-
-    def clear_space(space)
-      apps = space.apps
-      services = space.service_instances
-
-      return true if apps.empty? && services.empty?
-
-      unless force?
-        line "This space is not empty!"
-        line
-        line "apps: #{name_list(apps)}"
-        line "service: #{name_list(services)}"
-        line
-
-        return unless input[:recursive]
-      end
-
-      apps.each do |a|
-        invoke :delete, :app => a, :really => true
-      end
-
-      services.each do |i|
-        invoke :delete_service, :service => i, :really => true
-      end
-
-      true
     end
 
     private
-
-    def ask_spaces(org)
-      spaces = org.spaces
-      fail "No spaces." if spaces.empty?
-
-      [ask("Which space in #{c(org.name, :name)}?", :choices => spaces,
-           :display => proc(&:name))]
-    end
 
     def ask_really(space)
       ask("Really delete #{c(space.name, :name)}?", :default => false)
---
+++ spec/cf/cli/space/delete_spec.rb
@@ -0,0 +1,91 @@
+require 'spec_helper'
+require "cf/cli/space/delete"
+
+describe CF::Space::Delete do
+  describe 'metadata' do
+    let(:command) { Mothership.commands[:delete_space] }
+
+    describe 'command' do
+      subject { command }
+      its(:description) { should eq "Delete a space and its contents" }
+      it { expect(Mothership::Help.group(:spaces)).to include(subject) }
+    end
+
+    include_examples 'inputs must have descriptions'
+
+    describe 'arguments' do
+      subject { command.arguments }
+      it 'has the correct argument order' do
+        should eq([
+          {:type => :splat, :value => nil, :name => :spaces}
+        ])
+      end
+    end
+  end
+
+  describe "running the command" do
+    let(:space) { fake :space, :name => "some_space_name" }
+    let(:space_2) { fake :space, :name => "some_other_space_name" }
+    let(:spaces) { [space, space_2] }
+    let(:organization) { fake(:organization, :spaces => spaces, :name => "MyOrg") }
+
+    let(:client) { fake_client(:current_organization => organization, :spaces => spaces) }
+
+    subject { capture_output { cf %W[delete-space some_space_name some_other_space_name --quiet --force] } }
+
+    before do
+      any_instance_of described_class do |cli|
+        stub(cli).client { client }
+
+        stub(cli).check_logged_in
+        stub(cli).check_target
+        any_instance_of(CF::Populators::Organization, :populate_and_save! => organization)
+      end
+      stub(space).delete!
+      stub(space_2).delete!
+    end
+
+    it "deletes all named spaces" do
+      mock(space).delete!
+      mock(space_2).delete!
+
+      subject
+    end
+
+    context "without the force parameter" do
+      subject { cf %W[delete-space some_space_name some_other_space_name --quiet] }
+      it "confirms deletion of each space and deletes them" do
+        mock(space).delete!
+        dont_allow(space_2).delete!
+        mock_ask("Really delete #{space.name}?", {:default => false}) { true }
+        mock_ask("Really delete #{space_2.name}?", {:default => false}) { false }
+
+        subject
+      end
+    end
+
+    context "when deleting the current space" do
+      it "warns the user what they've done" do
+        stub(client).current_space { space }
+
+        subject
+        expect(output).to say("The space that you were targeting has now been deleted. Please use `cf target -s SPACE_NAME` to target a different one.")
+      end
+    end
+
+    context "when a space fails to delete" do
+      before do
+        stub(space).delete! { raise CFoundry::APIError.new("We don't delete children.", 40005) }
+        subject
+      end
+
+      it "shows the error message" do
+        expect(output).to say "We don't delete children."
+      end
+
+      it "informs the user of how to recursively delete" do
+        expect(output).to say "If you want to delete the space along with all dependent objects, rerun the command with the '--recursive' flag."
+      end
+    end
+  end
+end
