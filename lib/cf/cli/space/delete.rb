21499daadb7c9242ddc4199caaebfe2980cf0d1a
Ryan Spore & Will Read <pair+ryan+will@pivotallabs.com>
2013-04-30 10:57:17 -0700

    Only accept one space at a time to simplify output and match delete-org
    Which one succeeded? Which one failed? Which one was the current space?

--- lib/cf/cli/space/delete.rb
+++ lib/cf/cli/space/delete.rb
@@ -7,8 +7,8 @@ module CF::Space
     input :organization, :desc => "Space's organization",
       :aliases => ["--org", "-o"], :from_given => by_name(:organization),
       :default => proc { client.current_organization }
-    input :spaces, :desc => "Spaces to delete", :argument => :splat,
-      :singular => :space, :from_given => space_by_name
+    input :space, :desc => "Space to delete", :argument => true,
+      :from_given => space_by_name
     input :recursive, :desc => "Delete recursively", :alias => "-r",
       :default => false, :forget => true
     input :warn, :desc => "Show warning if it was the last space",
@@ -17,35 +17,30 @@ module CF::Space
       :default => proc { force? || interact }
 
     def delete_space
-      spaces = input[:spaces, org]
+      space = input[:space, org]
 
-      deleted_current = false
+      return unless input[:really, space]
 
-      spaces.each do |space|
-        next unless input[:really, space]
+      deleting_current_space = (space == client.current_space)
 
-        deleted_current ||= (space == client.current_space)
-
-        begin
-          with_progress("Deleting space #{c(space.name, :name)}") do
-            if input[:recursive]
-              space.delete!(:recursive => true)
-            else
-              space.delete!
-            end
+      begin
+        with_progress("Deleting space #{c(space.name, :name)}") do
+          if input[:recursive]
+            space.delete!(:recursive => true)
+          else
+            space.delete!
           end
-        rescue CFoundry::AssociationNotEmpty => boom
-          line
-          line c(boom.description, :bad)
-          line c("If you want to delete the space along with all dependent objects, rerun the command with the #{b("'--recursive'")} flag.", :bad)
-          exit_status(1)
-          return
-        end
-      end
 
-      if deleted_current
+          if deleting_current_space
+            line
+            line c("The space that you were targeting has now been deleted. Please use #{b("`cf target -s SPACE_NAME`")} to target a different one.", :warning)
+          end
+        end
+      rescue CFoundry::AssociationNotEmpty => boom
         line
-        line c("The space that you were targeting has now been deleted. Please use #{b("`cf target -s SPACE_NAME`")} to target a different one.", :warning)
+        line c(boom.description, :bad)
+        line c("If you want to delete the space along with all dependent objects, rerun the command with the #{b("'--recursive'")} flag.", :bad)
+        exit_status(1)
       end
     end
 
--- spec/cf/cli/space/delete_spec.rb
+++ spec/cf/cli/space/delete_spec.rb
@@ -12,26 +12,14 @@ describe CF::Space::Delete do
     end
 
     include_examples 'inputs must have descriptions'
-
-    describe 'arguments' do
-      subject { command.arguments }
-      it 'has the correct argument order' do
-        should eq([
-          {:type => :splat, :value => nil, :name => :spaces}
-        ])
-      end
-    end
   end
 
   describe "running the command" do
     let(:space) { fake :space, :name => "some_space_name" }
-    let(:space_2) { fake :space, :name => "some_other_space_name" }
-    let(:spaces) { [space, space_2] }
-    let(:organization) { fake(:organization, :spaces => spaces, :name => "MyOrg") }
+    let(:organization) { fake(:organization, :spaces => [space], :name => "MyOrg") }
+    let(:client) { fake_client(:current_organization => organization, :spaces => [space]) }
 
-    let(:client) { fake_client(:current_organization => organization, :spaces => spaces) }
-
-    subject { capture_output { cf %W[delete-space some_space_name some_other_space_name --quiet --force] } }
+    subject { capture_output { cf %W[delete-space some_space_name --quiet --force] } }
 
     before do
       any_instance_of described_class do |cli|
@@ -42,23 +30,14 @@ describe CF::Space::Delete do
         any_instance_of(CF::Populators::Organization, :populate_and_save! => organization)
       end
       stub(space).delete!
-      stub(space_2).delete!
     end
 
-    it "deletes all named spaces" do
-      mock(space).delete!
-      mock(space_2).delete!
-
-      subject
-    end
 
     context "without the force parameter" do
-      subject { cf %W[delete-space some_space_name some_other_space_name --quiet] }
+      subject { cf %W[delete-space some_space_name --quiet] }
       it "confirms deletion of each space and deletes them" do
         mock(space).delete!
-        dont_allow(space_2).delete!
         mock_ask("Really delete #{space.name}?", {:default => false}) { true }
-        mock_ask("Really delete #{space_2.name}?", {:default => false}) { false }
 
         subject
       end
