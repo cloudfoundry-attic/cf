f8c23cc4e3c927d69b7aa3b186b074457a2b0041
Andreas Maier & Will Read <pair+amaier+will@pivotallabs.com>
2013-05-24 14:54:43 -0700

    [#50531707] Change rds_mysql to human readable name

--- spec/features/push_flow_spec.rb
+++ spec/features/push_flow_spec.rb
@@ -65,7 +65,7 @@ if ENV['CF_V2_RUN_INTEGRATION']
 
           # create a service here
           expect(runner).to say "What kind?>"
-          runner.send_keys "rds_mysql"
+          runner.send_keys "rds-mysql"
 
           expect(runner).to say "Name>"
           runner.send_keys service_name
