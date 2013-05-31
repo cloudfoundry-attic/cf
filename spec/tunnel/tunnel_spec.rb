3d2c3b0903f4675d6d7cbcaf670575124d3bfb28
Dennis Schmidt <pair+dschmidt@pivotallabs.com>
2013-05-31 15:27:48 -0700

    remove rr survivors

--- spec/tunnel/tunnel_spec.rb
+++ spec/tunnel/tunnel_spec.rb
@@ -38,14 +38,14 @@ describe CFTunnel do
       let(:host) { "caldecott" }
 
       before do
-        stub(subject).random_helper_url { "caldecott" }
-        stub(client).app { app }
-        stub(client).current_space { space }
-        stub(client).domains { [fake(:domain, :owning_organization => fake(:organization)), domain] }
+       subject.stub(:random_helper_url) { "caldecott" }
+       client.stub(:app) { app }
+       client.stub(:current_space) { space }
+       client.stub(:domains) { [fake(:domain, :owning_organization => fake(:organization)), domain] }
       end
 
       it "adds a new route to caldecott app" do
-        mock(app).create_route(:domain => domain, :host => host, :space => space)
+        app.should_receive(:create_route).with(:domain => domain, :host => host, :space => space)
 
         subject.send("push_helper", "this is a token")
       end
