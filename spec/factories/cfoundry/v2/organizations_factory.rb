e44e6c5551fc8f79837d6b2c0cb00c24eb2ce5e0
David Sabeti and Ryan Spore <pair+dsabeti+ryan@pivotallabs.com>
2013-07-10 17:42:00 -0700

    add sane defaults for spaces and users to org factory

--- spec/factories/cfoundry/v2/organizations_factory.rb
+++ spec/factories/cfoundry/v2/organizations_factory.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :organization, :class => CFoundry::V2::Organization do
     sequence(:guid) { |n| "organization-guid-#{n}" }
     sequence(:name) { |n| "organization-name-#{n}" }
+    users { [build(:user)] }
+    spaces { [build(:space)] }
 
     ignore do
       client { FactoryGirl.build(:client) }
