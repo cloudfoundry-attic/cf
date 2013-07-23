aa329edad6270a5da1a3841db169dfe0b97c03b7
David Sabeti and Jesse Zhang <pair+dsabeti+jz@pivotallabs.com>
2013-07-23 10:00:35 -0700

    Remove dead code

--- spec/features/services_spec.rb
+++ spec/features/services_spec.rb
@@ -8,8 +8,6 @@ if ENV['CF_V2_RUN_INTEGRATION']
 
     describe "creating a service" do
       describe "when the user leaves the line blank for a plan" do
-        let(:services) { [selected_service] }
-
         it "re-prompts for the plan" do
           BlueShell::Runner.run("#{cf_bin} create-service") do |runner|
             expect(runner).to say "What kind?"
