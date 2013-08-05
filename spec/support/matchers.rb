b775fe76017952a0c7eac2742305469a70d09ef5
Jeff Schnitzer and Jesse Zhang <pair+jschnitzer+jz@pivotallabs.com>
2013-08-05 15:02:40 -0700

    has_label usable as argument matcher

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -1,5 +1,16 @@
-RSpec::Matchers.define :has_label do |expected|
-  match do |actual|
-    actual.label == expected
+class HasLabelMatcher
+  def initialize(expected_label)
+    @expected = expected_label
   end
+  def failure_message_for_should
+    "#{actual} does not have label #{@expected}"
+  end
+  def ==(actual)
+    actual.label == @expected
+  end
+  alias_method :matches?, :==
+end
+
+def has_label(expected_label)
+  HasLabelMatcher.new(expected_label)
 end
