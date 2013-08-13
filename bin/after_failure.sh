eb351f91067eccb89b33759b4b0fd444a1ba51e1
Jesse Zhang and Matt Royal <pair+jz+mroyal@pivotallabs.com>
2013-08-13 15:25:06 -0700

    Debug log output to ensure logs aren't truncated

--- bin/after_failure.sh
+++ bin/after_failure.sh
@@ -2,4 +2,8 @@
 
 # cat any relevant logs
 
+echo "=========================================="
+echo "Here is the line count for each log file:"
+wc -l tmp/log/*
+echo "=========================================="
 tail -n +1 tmp/log/*
