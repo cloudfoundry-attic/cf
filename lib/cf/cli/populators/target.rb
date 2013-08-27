f3faeae3df0469793893cc22aa7c977564589d10
David Julia and Jimmy Da <pair+djulia+jda@pivotallabs.com>
2013-08-27 11:11:02 -0700

    Do not attempt to populate a space if there are no orgs.
      Attempting to populate a space  was causing cf to crash
      when trying to login to cloud foundry deployments that had
      no orgs.
    
      +Also cleaned up the specs around the Target Populator to make it
      clear that an Organization Populator returns a CFoundry Organization.
    
      [Fixes #55842402]

--- lib/cf/cli/populators/target.rb
+++ lib/cf/cli/populators/target.rb
@@ -6,7 +6,8 @@ module CF
     class Target < Base
       def populate_and_save!
         organization = CF::Populators::Organization.new(input).populate_and_save!
-        CF::Populators::Space.new(input, organization).populate_and_save!
+
+        CF::Populators::Space.new(input, organization).populate_and_save! unless organization.nil?
       end
     end
   end
--- spec/cf/cli/populators/target_spec.rb
+++ spec/cf/cli/populators/target_spec.rb
@@ -1,25 +1,39 @@
 require "spec_helper"
 
 module CF
-  module Populators
-    describe Target do
+    describe Populators::Target do
       describe "#populate_and_save!" do
         let(:input) { double(:input) }
-        let(:organization) { double(:organization) }
-        let(:space) { double(:space) }
+        let(:organization_populator) { double(Populators::Organization) }
+        let(:space_populator) { double(Populators::Space) }
 
         def execute_populate_and_save
-          Target.new(input).populate_and_save!
+          Populators::Target.new(input).populate_and_save!
         end
 
-        it "uses a organization then a space populator" do
-          organization.should_receive(:populate_and_save!).and_return(organization)
-          space.should_receive(:populate_and_save!)
-          Organization.should_receive(:new).with(input).and_return(organization)
-          Space.should_receive(:new).with(input, organization).and_return(space)
+        context 'when there are no orgs' do
+          it 'does not try to populate the space' do
+            Populators::Organization.stub(:new).and_return(organization_populator)
+            organization_populator.stub(:populate_and_save!).and_return(nil)
+
+            Populators::Space.stub(:new).and_return(space_populator)
+            space_populator.should_not receive(:populate_and_save!)
+
+            execute_populate_and_save
+          end
+        end
+
+        it "uses a organization_populator then a space_populator populator" do
+          Populators::Organization.should_receive(:new).with(input).and_return(organization_populator)
+
+          cfoundry_organization = double(CFoundry::V2::Organization)
+          organization_populator.should_receive(:populate_and_save!).and_return(cfoundry_organization)
+
+          Populators::Space.should_receive(:new).with(input, cfoundry_organization).and_return(space_populator)
+          space_populator.should_receive(:populate_and_save!)
+
           execute_populate_and_save
         end
       end
     end
-  end
 end
